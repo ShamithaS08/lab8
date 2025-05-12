399a92d2fdd769b97215e19e4ae6c0f73b5116d8
ShamithaS08 <shamithashankar0806@gmail.com>
2025-05-12 18:02:22 +0530

    first commit

---
+++ lab8/src/main/java/com/ShamithaS08/App.java
@@ -0,0 +1,13 @@
+package com.ShamithaS08;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
